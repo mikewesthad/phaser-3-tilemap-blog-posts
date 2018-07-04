5a8ab0f0bb3822cd9502a9f8759692da34d4ccc9
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-03 21:40:39 -0500

    First post draft

---
+++ posts/post-1/images/loading-from-tiled/index.js
@@ -0,0 +1,40 @@
+/**
+ * Author: Michael Hadley, mikewesthad.com
+ * Codepen Link:
+ * Github Source:
+ * Asset Credits:
+ *  - Tuxemon, https://github.com/Tuxemon/Tuxemon
+ */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  parent: "phaser-container",
+  pixelArt: true,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+const game = new Phaser.Game(config);
+let controls;
+
+function preload() {
+  this.load.image("tiles", "../assets/tilesets/tuxmon-sample-32px-extruded.png");
+  this.load.tilemapTiledJSON("map", "../assets/tilemaps/tuxemon-town.json");
+}
+
+function create() {
+  const map = this.make.tilemap({ key: "map" });
+
+  const tileset = map.addTilesetImage("tuxmon-sample-32px-extruded", "tiles");
+
+  const belowLayer = map.createStaticLayer("Below Player", tileset, 0, 0);
+  const worldLayer = map.createStaticLayer("World", tileset, 0, 0);
+  const aboveLayer = map.createStaticLayer("Above Player", tileset, 0, 0);
+}
+
+function update(time, delta) {}
---
+++ posts/post-1/images/tiled-demo/buch-tileset.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="buch-tileset" tilewidth="48" tileheight="48" tilecount="247" columns="19">
+ <grid orientation="orthogonal" width="16" height="16"/>
+ <image source="buch-tileset-3x.png" width="912" height="624"/>
+</tileset>
