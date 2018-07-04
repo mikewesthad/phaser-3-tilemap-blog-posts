5da715f11e39717e26399348164bbab1b379eb25
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-04 09:07:14 -0500

    Comment and reorg 05-physics example

--- code/05-physics/index.js
+++ code/05-physics/index.js
@@ -28,10 +28,17 @@ const config = {
 const game = new Phaser.Game(config);
 let cursors;
 let player;
+let showDebug = false;
 
 function preload() {
   this.load.image("tiles", "../assets/tilesets/tuxmon-sample-32px-extruded.png");
   this.load.tilemapTiledJSON("map", "../assets/tilemaps/tuxemon-town.json");
+
+  // An atlas is a way to pack multiple images together into one texture. I'm using it to load all
+  // the player animations (walking left, walking right, etc.) in one image. For more info see:
+  //  https://labs.phaser.io/view.html?src=src/animation/texture%20atlas%20animation.js
+  // If you don't use an atlas, you can do the same thing with a spritesheet, see:
+  //  https://labs.phaser.io/view.html?src=src/animation/single%20sprite%20sheet.js
   this.load.atlas("atlas", "../assets/atlas/atlas.png", "../assets/atlas/atlas.json");
 }
 
@@ -46,43 +53,65 @@ function create() {
   const belowLayer = map.createDynamicLayer("Below Player", tileset, 0, 0);
   const worldLayer = map.createDynamicLayer("World", tileset, 0, 0);
   const aboveLayer = map.createDynamicLayer("Above Player", tileset, 0, 0);
-  aboveLayer.setDepth(10);
+
   worldLayer.setCollisionByProperty({ collides: true });
+
+  // By default, everything gets depth sorted on the screen in the order we created things. Here, we
+  // want the "Above Player" layer to sit on top of the player, so we explicitly give it a depth.
+  // Higher depths will sit on top of lower depth objects.
+  aboveLayer.setDepth(10);
 
   // Object layers in Tiled let you embed extra info into a map - like a spawn point or custom
   // collision shapes. In the tmx file, there's an object layer with a point named "Spawn Point"
   const spawnPoint = map.findObject("Objects", obj => obj.name === "Spawn Point");
 
+  // Create a sprite with physics enabled via the physics system. The image used for the sprite has
+  // a bit of whitespace, so I'm using setSize & setOffset to control the size of the player's body.
   player = this.physics.add
     .sprite(spawnPoint.x, spawnPoint.y, "atlas", "misa-front")
     .setSize(30, 40)
     .setOffset(0, 24);
 
+  // Watch the player and worldLayer for collisions, for the duration of the scene:
   this.physics.add.collider(player, worldLayer);
 
-  // Phaser supports multiple cameras, but you can access the default camera like this:
+  // Create the player's walking animations from the texture atlas. These are stored in the global
+  // animation manager so any sprite can access them.
+  const anims = this.anims;
+  anims.create({
+    key: "misa-left-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-right-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-front-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "misa-back-walk",
+    frames: anims.generateFrameNames("atlas", { prefix: "misa-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
   const camera = this.cameras.main;
-
   camera.startFollow(player);
-
-  // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
   camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
 
   cursors = this.input.keyboard.createCursorKeys();
 
-  const debugGraphics = this.add
-    .graphics()
-    .setAlpha(0.75)
-    .setDepth(20);
-  worldLayer.renderDebug(debugGraphics, {
-    tileColor: null, // Color of non-colliding tiles
-    collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
-    faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
-  });
-
   // Help text that has a "fixed" position on the screen
   this.add
-    .text(16, 16, "Arrow keys to scroll", {
+    .text(16, 16, 'Arrow keys to move\nPress "D" to show hitboxes', {
       font: "18px monospace",
       fill: "#000000",
       padding: { x: 20, y: 10 },
@@ -91,52 +120,21 @@ function create() {
     .setScrollFactor(0)
     .setDepth(30);
 
-  this.anims.create({
-    key: "misa-left-walk",
-    frames: this.anims.generateFrameNames("atlas", {
-      prefix: "misa-left-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
+  // Debug graphics
+  this.input.keyboard.once("keydown_D", event => {
+    // Turn on physics debugging to show player's hitbox
+    this.physics.world.createDebugGraphic();
 
-  this.anims.create({
-    key: "misa-right-walk",
-    frames: this.anims.generateFrameNames("atlas", {
-      prefix: "misa-right-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: "misa-front-walk",
-    frames: this.anims.generateFrameNames("atlas", {
-      prefix: "misa-front-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
-  });
-
-  this.anims.create({
-    key: "misa-back-walk",
-    frames: this.anims.generateFrameNames("atlas", {
-      prefix: "misa-back-walk.",
-      start: 0,
-      end: 3,
-      zeroPad: 3
-    }),
-    frameRate: 10,
-    repeat: -1
+    // Create worldLayer collision graphic above the player, but below the help text
+    const graphics = this.add
+      .graphics()
+      .setAlpha(0.75)
+      .setDepth(20);
+    worldLayer.renderDebug(graphics, {
+      tileColor: null, // Color of non-colliding tiles
+      collidingTileColor: new Phaser.Display.Color(243, 134, 48, 255), // Color of colliding tiles
+      faceColor: new Phaser.Display.Color(40, 39, 37, 255) // Color of colliding face edges
+    });
   });
 }
 
