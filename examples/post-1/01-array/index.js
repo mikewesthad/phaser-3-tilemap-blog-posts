cfb8b8cb1f4d49c004e21915de5f4c61ad831476
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-04 10:13:24 -0500

    Reorg assets and remove unused files

---
+++ examples/post-1/01-array/index.js
@@ -0,0 +1,48 @@
+/**
+ * Author: Michael Hadley, mikewesthad.com
+ * Codepen Link:
+ * Github Source:
+ * Asset Credits:
+ *  - Nintendo Mario Tiles, Educational use only
+ */
+
+const config = {
+  type: Phaser.AUTO,
+  width: 171,
+  height: 160,
+  zoom: 4, // Since we're working with 16x16 pixel tiles, let's scale up the canvas by 4x
+  pixelArt: true, // Force the game to scale images up crisply
+  parent: "phaser-container",
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
+
+const game = new Phaser.Game(config);
+
+function preload() {
+  this.load.image("mario-tiles", "../assets/tilesets/super-mario-tiles.png");
+}
+
+function create() {
+  // Load a map from a 2D array of tile indices
+  const level = [
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+    [  0,  1,  2,  3,  0,  0,  0,  1,  2,  3,  0 ],
+    [  0,  5,  6,  7,  0,  0,  0,  5,  6,  7,  0 ],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+    [  0,  0,  0, 14, 13, 14,  0,  0,  0,  0,  0 ],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 ],
+    [  0,  0, 14, 14, 14, 14, 14,  0,  0,  0, 15 ],
+    [  0,  0,  0,  0,  0,  0,  0,  0,  0, 15, 15 ],
+    [ 35, 36, 37,  0,  0,  0,  0,  0, 15, 15, 15 ],
+    [ 39, 39, 39, 39, 39, 39, 39, 39, 39, 39, 39 ]
+  ];
+
+  // When loading from an array, make sure to specify the tileWidth and tileHeight
+  const map = this.make.tilemap({ data: level, tileWidth: 16, tileHeight: 16 });
+  const tiles = map.addTilesetImage("mario-tiles");
+  const layer = map.createStaticLayer(0, tiles, 0, 0);
+}
---
+++ examples/post-1/assets/sources.md
@@ -0,0 +1,5 @@
+# Sources
+
+[Super Mario Tileset](http://rmrk.net/index.php?topic=37002.0) by Arrow
+[Tuxemon](https://github.com/Tuxemon/Tuxemon) tileset, sprites and level by the Tuxemon team
+[Cat Astro Phi](http://www.photonstorm.com/games/cat-astro-phi) tileset and level by Rich Davey & Ilija Melentijević 
