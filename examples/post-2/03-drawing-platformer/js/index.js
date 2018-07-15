5c46c75f1e6f6bd7fb60a45b5831fae47bdda28f
Michael Hadley <michaelwesthadley@gmail.com>
2018-07-14 19:42:53 -0500

    Enable pixel art to fix bleed on spike sprites

--- examples/post-2/03-drawing-platformer/js/index.js
+++ examples/post-2/03-drawing-platformer/js/index.js
@@ -11,7 +11,7 @@ const config = {
   width: 800,
   height: 600,
   parent: "game-container",
-  pixelArt: false,
+  pixelArt: true,
   backgroundColor: "#1d212d",
   scene: PlatformerScene,
   physics: {
