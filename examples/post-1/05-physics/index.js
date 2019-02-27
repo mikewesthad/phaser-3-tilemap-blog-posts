226b3b9fbe3367b511c87e59d0f03d7a8225391d
Michael Hadley <michaelwesthadley@gmail.com>
2019-02-26 19:56:14 -0600

    prettier the code

--- examples/post-1/05-physics/index.js
+++ examples/post-1/05-physics/index.js
@@ -78,25 +78,45 @@ function create() {
   const anims = this.anims;
   anims.create({
     key: "misa-left-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-left-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-left-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
     frameRate: 10,
     repeat: -1
   });
   anims.create({
     key: "misa-right-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-right-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-right-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
     frameRate: 10,
     repeat: -1
   });
   anims.create({
     key: "misa-front-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-front-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-front-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
     frameRate: 10,
     repeat: -1
   });
   anims.create({
     key: "misa-back-walk",
-    frames: anims.generateFrameNames("atlas", { prefix: "misa-back-walk.", start: 0, end: 3, zeroPad: 3 }),
+    frames: anims.generateFrameNames("atlas", {
+      prefix: "misa-back-walk.",
+      start: 0,
+      end: 3,
+      zeroPad: 3
+    }),
     frameRate: 10,
     repeat: -1
   });
